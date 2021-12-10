fef7c5479bd23647ab247f021249e14448f7e36d
Barbara <broney@mills.edu>
2021-12-09 16:44:58 -0800

    Initial commit

---
+++ src/GraphNode.java
@@ -0,0 +1,17 @@
+
+
+import java.util.HashMap;
+
+public class GraphNode {
+    private String location;
+    private HashMap<GraphNode, Integer> outboundEdges;
+
+    // Alternative to using a map for the edges:
+    // private ArrayList<GraphNode> neighbors;
+    // private ArrayList<Integer> distances;
+
+    void addEdge(String otherNode, int distance) {
+
+    }
+
+}
